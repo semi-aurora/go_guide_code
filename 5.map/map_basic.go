1607eb1c8872a3ad026b1c92e3548e7b8a125815
supine-win <mi-autumn@hotmail.com>
2019-12-04 23:47:23 +0800

    z2-20191204-night

--- 5.map/map_basic.go
+++ 5.map/map_basic.go
@@ -1,5 +1,51 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"sort"
+)
 
+var (
+	barVal = map[string]int{"alpha": 34, "bravo": 56, "charlie": 23,
+		"delta": 87, "echo": 56, "foxtrot": 12,
+		"golf": 34, "hotel": 16, "indio": 87,
+		"juliet": 65, "kili": 43, "lima": 98}
+)
+
+func main() {
+
+	//sort map
+	fmt.Println("unsorted:")
+	for k, v := range barVal {
+		fmt.Printf("Key: %v, Value: %v / ", k, v)
+	}
+	keys := make([]string, len(barVal))
+	i := 0
+	for k, _ := range barVal {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println()
+	fmt.Println("sorted:")
+	for _, k := range keys {
+		fmt.Printf("Key: %v, Value: %v / ", k, barVal[k])
+	}
+
+	//exchange the key and value of map
+	var (
+		barVal = map[string]int{"alpha": 34, "bravo": 56, "charlie": 23,
+			"delta": 87, "echo": 56, "foxtrot": 12,
+			"golf": 34, "hotel": 16, "indio": 87,
+			"juliet": 65, "kili": 43, "lima": 98}
+	)
+
+	invMap := make(map[int]string, len(barVal))
+	for k, v := range barVal {
+		invMap[v] = k
+	}
+	fmt.Println("inverted:")
+	for k, v := range invMap {
+		fmt.Printf("Key: %v, Value: %v / ", k, v)
+	}
 }
